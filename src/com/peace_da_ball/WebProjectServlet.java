0849eaee2b6047b00322aafa0a72db951c731116
Anatoly <Anatoly-Vinnikov>
2017-04-05 18:48:43 +0200

    Lecture
    Added lecture

--- src/com/peace_da_ball/WebProjectServlet.java
+++ src/com/peace_da_ball/WebProjectServlet.java
@@ -11,6 +11,7 @@ public class WebProjectServlet extends HttpServlet {
 	Logic codesAndSum = new Logic();
 	Multiplication multiplication = new Multiplication();
 	NumberSystems numberSystems = new NumberSystems();
+	Lecture lecture = new Lecture();
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {		
 		resp.setContentType("text/html; charset=windows-1251");
@@ -27,6 +28,9 @@ public class WebProjectServlet extends HttpServlet {
         	break;
         case "2":
         	out.print(numberSystems.generate(request[1]));
+        	break;
+        case "3":
+        	out.print(lecture.getLecture(request[1]));
         	break;
         }
 	}
